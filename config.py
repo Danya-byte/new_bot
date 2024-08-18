9e5f3bec50c6a4a25d91557db7808e13add72559
Danya <danya001406@gmail.com>
2024-08-18 11:43:53 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-TOKEN = '7225528625:AAG5NkKDH0WzZJ7FhwA773Tb_TRKZ-OS9Cg'
-PAYMENTS_PROVIDER_TOKEN = '401643678:TEST:d8caa356-a508-43ea-a0db-6cf3f41d8ee4'
-TIME_MACHINE_IMAGE_URL = 'https://example.com/time_machine.jpg'
-ADMIN_USER_ID = 1940359844  # ID администратора.
-DATABASE_PATH = 'burgers.db'
-GITLAB_ACCESS_TOKEN = 'glpat-kKtibzPubYTr-Zsm6BbW'
